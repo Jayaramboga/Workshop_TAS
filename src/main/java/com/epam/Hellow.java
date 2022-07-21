7c5916a9c4360e2e2b7606234b92db85261b9cda
Jayaram_Boga <jayaram_boga@epam.com>
2022-07-21 12:48:24 +0530

    Initial commit

---
+++ src/main/java/com/epam/Hellow.java
@@ -0,0 +1,9 @@
+package com.epam;
+
+public class Hellow {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+    }
+    }
+
---
+++ src/test/java/com/epam/Smoketest.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+import org.junit.jupiter.api.Test;
+
+public class Smoketest {
+@Test
+    public void test()
+{
+    System.out.println("hellow from test");
+}
+}
